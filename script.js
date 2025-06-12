7f7a827bb206819bd23e24c722a4398ea4962ac7
Kaique Sasaki de Campos <48853839864@escola927.edu>
2025-06-12 08:09:17 -0300

    teste bru

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 //definir variaveis constantes para uso no código
 
-const btnAdd = document.getElementById('add');
+const btnadd = document.getElementById('btnadd');
 const tarefa = document.getElementById('tarefa');
 const taskList = document.getElementById('taskList');
 const titulo = document.getElementById('titulo');
@@ -15,8 +15,36 @@ if(nome =='') {
 else{
     titulo.innerHTML = `Lista de Tarefas do(a) ${nome}`;
 }
-btnAdd.addEventListener('click', criaTarefa);
+btnadd.addEventListener('click', criaTarefa);
 
 function criaTarefa() {
-   
+    const listItem = document.createElement('li');
+    listItem.textContent = tarefa.value;
+    taskList.appendChild(listItem)
+    //criar botão de deletar tarefa
+    const removeButton = document.createElement('button');
+    removeButton.id = "remove";
+    removeButton.textContent = 'X';
+    listItem.appendChild(removeButton);
+
+    removeButton.addEventListener("click", function(){
+        taskList.removeChild(listItem);
+    });
+
+    //criar botão de concluir tarefa
+    const concluirButton = document.createElement('button');
+    concluirButton.id = "conclui";
+    concluirButton.textContent = '✓';
+
+    //listItem.appendChild(concluirButton);
+
+    let buttonsItem = document.createElement('div');
+    buttonsItem.classList.toggle('buttonsItem');
+    listItem.appendChild(buttonsItem);
+    buttonsItem.appendChild(concluirButton);
+    buttonsItem.appendChild(removeButton);
+    
+    concluirButton.addEventListener("click", function(){
+        listItem.classList.toggle('completed');
+    });
 }
